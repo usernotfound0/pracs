12ca10758eb5214325bc1786ff8f44ed04b92864
usernotfound0 <57027383+usernotfound0@users.noreply.github.com>
2019-11-04 09:58:26 +0530

    Final Commit

--- MC/8. Orthogonal Walsh Code in CDMA/mc8.m
+++ MC/8. Orthogonal Walsh Code in CDMA/mc8.m
@@ -1,9 +1,23 @@
-clc
 clear all
 close all
-
-length = input('enter length of number of bits of power 2:');
-walsh = hadamard(length)
-codelength = input('Enter codelenth required:');
-userno = input('Enter the number of users required:');
-orth_mat = walsh(1:userno,1:codelength)
+A=[1 1 0 1 0 0 1];
+B=[1 1 0 0 0 0 0];
+c=A.*B;
+d=sum(c);
+if (mod(d,2)==0)
+ disp("Orthogonal");
+else
+ disp("Not Orthogonal");
+end
+P=[-1 1 -1 1 -1 1 -1 1];
+Q=[-1 -1 1 1 -1 -1 1 1];
+R=[-1 1 1 -1 -1 1 1 -1];
+S=[-1 -1 -1 -1 1 1 1 1];
+l=(P+Q+R+S).*R; %try for R; P+Q; P+Q+R as well
+m=sum(l);
+n=m/8;
+if (n==1)
+ disp("Message Received")
+else
+ disp("Message Lost")
+end
